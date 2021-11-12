ab01f13e862cf0b1ca487203471ce1132a44440b
acerockracer <ace_rockracer@yahoo.com>
2021-11-12 08:21:20 -0600

    Initial Commit

---
+++ Rotten games/Rotten_gamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rotten_gamesApp.swift
+//  Rotten games
+//
+//  Created by AMStudent on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Rotten_gamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
